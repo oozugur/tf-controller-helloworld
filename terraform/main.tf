f277a3155ffa7c42e15ef5f8ba0ad29e01acf2ae
Onur Ozugur <onurozugur@gmail.com>
2022-08-09 14:41:19 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,8 @@ resource "google_storage_bucket" "bucket" {
   name     = "ordt-bckt-12345"
   location = "EU"
 }
+
+resource "google_storage_bucket" "bucket" {
+  name     = "ordt-bckt-123425"
+  location = "EU"
+}
