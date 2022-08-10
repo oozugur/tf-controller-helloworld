fff13aefbd4d98afded983ac67adcd212cef5121
Onur Ozugur <onurozugur@gmail.com>
2022-08-10 13:24:14 +0300

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,6 @@
 resource "google_compute_network" "vpc_network" {
   project                 = "oredata-ops-demo"
-  name                    = "vpc-network2321"
+  name                    = "vpc-network2"
   auto_create_subnetworks = false
   mtu                     = 1460
 }
